bd1c69f185300374df51c54301983fff672cf2f0
parmarmilan089 <parmarmilan089@gmail.com>
2025-07-16 01:11:11 +0530

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Ranipur Tiger Reserve') }}</title>
+        <title>Ranipur Tiger Reserve</title>
 
         <!-- Favicon -->
         <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon">
