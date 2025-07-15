c85affe2fc43d32a3d16586e110e85e9b20b16dc
parmarmilan089 <parmarmilan089@gmail.com>
2025-07-16 01:07:47 +0530

    contact list

--- resources/views/admin/contact_forms/show.blade.php
+++ resources/views/admin/contact_forms/show.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
 
     <div class="py-6">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white p-6 shadow sm:rounded-lg">
             <div class="bg-white p-6 shadow-sm rounded space-y-4">
                 <div><strong>Name:</strong> {{ $form->name }}</div>
                 <div><strong>Email:</strong> {{ $form->email }}</div>
