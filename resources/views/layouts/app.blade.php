b15378c9ca18ca7a4dc13310fc8c7dffb7132a3c
parmarmilan089 <parmarmilan089@gmail.com>
2025-07-16 01:10:01 +0530

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
 
         <title>{{ config('app.name', 'Ranipur Tiger Reserve') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Ranipur Tiger Reserve') }}</title>    
+        <title>{{ config('app.name', 'Ranipur Tiger Reserve') }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
