911843f3cce28bb64f35ff5928160a8a549cdad4
parmarmilan089 <parmarmilan089@gmail.com>
2025-07-16 08:09:20 +0530

    “fix”

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -35,7 +35,7 @@
 
     <div class="boxed_wrapper">
         <!-- main header -->
-        <header class="main-header header-flex" id="myHeader">            
+        <header class="main-header header-flex" id="myHeader">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="{{ route('home') }}">
@@ -48,10 +48,10 @@
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
-                  
+
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
+                        <li class="nav-item {{ request()->routeIs('home') ? 'active' : '' }}">
                           <a class="nav-link" href="#about">About</a>
                         </li>
                         <li class="nav-item">
@@ -63,7 +63,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#reach">How to Reach</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item {{ request()->routeIs('contact') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('contact') }}">Contact</a>
                         </li>
                         <li class="nav-item">
@@ -79,11 +79,11 @@
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">Something else here</a>
                           </div>
-                        </li> -->                        
-                      </ul>                  
+                        </li> -->
+                      </ul>
                     </div>
                 </div>
-              </nav> 
+              </nav>
         </header>
         <!-- main-header end -->
 
